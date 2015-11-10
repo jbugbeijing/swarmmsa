14ff1db248de0f35cbd63033e8021288e17fed8e
Jim Ma <ema@redhat.com>
2015-11-10 10:44:39 +0800

    Add basic examples

---
+++ wildfly-swarm-examples/servlet/src/main/java/org/wildfly/swarm/examples/servlet/MyServlet.java
@@ -0,0 +1,23 @@
+package org.wildfly.swarm.examples.servlet;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.joda.time.DateTime;
+
+/**
+ * @author Bob McWhirter
+ */
+@WebServlet("/*")
+public class MyServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.getWriter().write("Howdy at " + new DateTime());
+    }
+}
