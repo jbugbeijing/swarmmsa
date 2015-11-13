7875a2901633b98c79a2850e9d1de8767e4c8aee
Jim Ma <ema@redhat.com>
2015-11-13 16:31:59 +0800

    Add slides;Add some classes to exercise

--- wildfly-swarm-examples/jpa-jaxrs-cdi-shrinkwrap/src/main/java/org/wildfly/swarm/examples/jpa/EmployeeResource.java
+++ wildfly-swarm-examples/jpa-jaxrs-cdi-shrinkwrap/src/main/java/org/wildfly/swarm/examples/jpa/EmployeeResource.java
@@ -3,9 +3,13 @@ package org.wildfly.swarm.examples.jpa;
 import javax.enterprise.context.ApplicationScoped;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 
 /**
  * @author Ken Finnigan
@@ -22,4 +26,14 @@ public class EmployeeResource {
     public Employee[] get() {
         return em.createNamedQuery("Employee.findAll", Employee.class).getResultList().toArray(new Employee[0]);
     }
+
+    //implement this
+    @POST
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response createEmployee(Employee employee) {
+    	//persistent employee
+    	return Response.ok().build();    	
+    }
+
 }
--- wildfly-swarm-examples/jpa-jaxrs-cdi/src/main/java/org/wildfly/swarm/examples/jpa/EmployeeResource.java
+++ wildfly-swarm-examples/jpa-jaxrs-cdi/src/main/java/org/wildfly/swarm/examples/jpa/EmployeeResource.java
@@ -1,9 +1,13 @@
 package org.wildfly.swarm.examples.jpa;
 
 import javax.inject.Inject;
+import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 
 /**
  * @author Ken Finnigan
@@ -18,5 +22,14 @@ public class EmployeeResource {
     @Produces("application/json")
     public Employee[] get() {
         return helper.getEntityManager().createNamedQuery("Employee.findAll", Employee.class).getResultList().toArray(new Employee[0]);
+    }
+
+    //implement this
+    @POST
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response createEmployee(Employee employee) {
+    	//persistent employee
+    	return Response.ok().build();    	
     }
 }
